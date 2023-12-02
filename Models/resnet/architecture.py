7caaf7b3c036dd1865415aa33ea58f4be6a057a8
Cyoger <akshat1sharma2@gmail.com>
2023-12-02 02:11:59 -0500

    this is a working architecture it runs with a low batch size

--- Models/resnet/architecture.py
+++ Models/resnet/architecture.py
@@ -5,35 +5,33 @@ from keras.models import Model
 
 class Network():
     
-    def build(self, num_unfreeze_layers=10):
+    def build(self, num_unfreeze_layers=0):
         # Define the input layer with the shape of your data
         input_layer = tf.keras.Input(shape=(32, 32, 3))
 
-        # Apply upsampling to match the expected input size of ResNet50
-        x = UpSampling2D(size=(7, 7))(input_layer)
-
         # Initialize the ResNet50 model
-        base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=x)
+        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
-        # Set the trainable status of the layers
+        for layer in base_model.layers:
+            layer.trainable = False
+            
         for layer in base_model.layers:
             if isinstance(layer, BatchNormalization):
                 layer.trainable = True
             else:
                 layer.trainable = False
+                
+        
+        x = UpSampling2D(size=(7, 7), interpolation='bilinear')(input_layer)
 
-        if num_unfreeze_layers > 0:
-            for layer in base_model.layers[-num_unfreeze_layers:]:
-                layer.trainable = True
-
-        # Apply the base model and continue building the model
-        x = base_model.output
+        x =  base_model(x)
         x = GlobalAveragePooling2D()(x)
-        x = Dense(256, activation='relu')(x)
         x = Dropout(0.25)(x)
+        x = Dense(256, activation='relu')(x)
         x = BatchNormalization()(x)
-        output = Dense(100, activation='softmax')(x)  # Adjust the number of classes as needed
-
+        output = Dense(100, activation='softmax')(x)
+        
         model = Model(inputs=input_layer, outputs=output)
-
+        
         return model
+        
