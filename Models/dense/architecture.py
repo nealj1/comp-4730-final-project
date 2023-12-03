d5183e166d3fb6194269dc1c3136deb36bb04f5d
nealj <nealj@uwindsor.ca>
2023-12-02 20:12:48 -0500

    Auto stash before merge of "main" and "origin/main"

--- Models/dense/architecture.py
+++ Models/dense/architecture.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Flatten, BatchNormalization, Dropout, UpSampling2D
 
 
 
@@ -9,10 +10,16 @@ class Network():
 
     def build(self):
         inputs = layers.Input(shape=(32,32,3)) 
-        # x = layers.Flatten()(inputs)
-        x = layers.Dense(16)(inputs)
-        x = layers.Dense(32)(x)
-        x = layers.Dense(64)(x)
+        x = layers.Flatten()(inputs)
+        x = layers.Dense(512, activation='relu')(x)
+        x = Dropout(0.25)(x)
+        x = BatchNormalization()(x)
+        x = layers.Dense(256, activation='relu')(x)
+        x = Dropout(0.25)(x)
+        x = BatchNormalization()(x)
+        x = layers.Dense(128, activation='relu')(x)
+        x = Dropout(0.25)(x)
+        x = BatchNormalization()(x)
         x = layers.Flatten()(x)
         outputs = layers.Dense(100, activation="softmax")(x)
         
